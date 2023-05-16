e7c630fc6a4d64fa0ce32d9daabad19f5c8176fb
jowidjaja <116462552+jowidjaja@users.noreply.github.com>
2023-05-16 10:14:15 -0400

    Add files via upload

---
+++ munteanlab.js
@@ -0,0 +1,15 @@
+const activePage = window.location.pathname;
+console.log(activePage);
+
+const navLinks = document.querySelectorAll ('nav a').
+forEach(link => {
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+    }
+})
+
+
+
+Fancybox.bind("[data-fancybox]", {
+// Your custom options
+});
